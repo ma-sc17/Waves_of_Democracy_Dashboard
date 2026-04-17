f8754f7e68383e7e9df15d14189d1f2a5bdc47ad
ma-sc17 <marco.schaefer117@gmail.com>
2026-04-17 16:42:22 +0200

    Bug fix

--- app.py
+++ app.py
@@ -37,11 +37,11 @@ fig_line = px.line(
     sub_country,
     x="year",
     y=selected_var,
-    line=dict(color="#97f554", width=2),
     title=f"{selected_var}: {country}"
 )
 
 fig_line.update_yaxes(range = [0,1])
+fig_line.update_traces(line_color="#97f554", line_width=3)
 
 st.plotly_chart(fig_line, width="stretch")
 
